4c3852d79bad429561a75e1184f373c7a8c28f0d
matan-ode <matanodentz@gmail.com>
2024-10-14 21:02:57 +0300

    Update Mail details

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -26,21 +26,17 @@ export function MailDetails() {
             })
     }
 
-
-    function onBack() {
-        navigate('/mail')
-    }
-
     if (!mail) return <div>Loading...</div>
 
     return (
         <section className="mail-details-container">
             <MailFilter />
             <section className="mail-details">
-                <h1>Mail subject: {mail.subject}</h1>
+                <h1>{mail.subject}</h1>
+                
                 <h1>mail body: {mail.body}</h1>
 
-                <button onClick={onBack}>Back</button>
+                <button><Link to={`/mail`}>Back</Link></button>
                 <section>
                     <button ><Link to={`/mail/${mail.prevMailId}`}>Prev Mail</Link></button>
                     <button ><Link to={`/mail/${mail.nextMailId}`}>Next Mail</Link></button>
