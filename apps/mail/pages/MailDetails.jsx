b02cbb07fe6c75c706d87ebd5602248b4fdedb36
matan-ode <matanodentz@gmail.com>
2024-10-14 23:51:11 +0300

    Update styling to MailDetails with arrows for paging

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -32,15 +32,22 @@ export function MailDetails() {
         <section className="mail-details-container">
             <MailFilter />
             <section className="mail-details">
-                <h1>{mail.subject}</h1>
-                
-                <h1>mail body: {mail.body}</h1>
-
-                <button><Link to={`/mail`}>Back</Link></button>
-                <section>
-                    <button ><Link to={`/mail/${mail.prevMailId}`}>Prev Mail</Link></button>
-                    <button ><Link to={`/mail/${mail.nextMailId}`}>Next Mail</Link></button>
+                <section className="tools-bar">
+                    <button><Link to={`/mail`}><img src="assets\img\mail-icons\arrow_back_24dp_666666_FILL1_wght400_GRAD0_opsz24.png" alt="arrow-back" /></Link></button>
+                    <section className="paging-btn-container">
+                        <button ><Link to={`/mail/${mail.prevMailId}`}><img src="assets\img\mail-icons\chevron_right_24dp_666666_FILL1_wght400_GRAD0_opsz24.png" alt="arrow-right" /></Link></button>
+                        <button ><Link to={`/mail/${mail.nextMailId}`}><img src="assets\img\mail-icons\chevron_left_24dp_666666_FILL1_wght400_GRAD0_opsz24.png" alt="arrow-left" /></Link></button>
+                    </section>
                 </section>
+                <section className="mail-main">
+                    <h1>{mail.subject}</h1>
+                    <div className="from-info">
+                        <img src="assets\img\mail-icons\profile-img.png" alt="user-img" />
+                        <h4>{mail.from}</h4>
+                    </div>
+                    <p>{mail.body}</p>
+                </section>
+
             </section>
         </section>
     )
