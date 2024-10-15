4bdeeb8e3b4b6a0cc4ce530b2ed0b29c1d18e3eb
matan-ode <matanodentz@gmail.com>
2024-10-15 12:25:34 +0300

    Add styling to mail compose

--- apps/mail/cmps/MailCompose.jsx
+++ apps/mail/cmps/MailCompose.jsx
@@ -12,12 +12,14 @@ export function MailCompose() {
                 {/* <form onSubmit={onSubmit}> */}
                 <form className="compose-main">
                     <input type="text" name="to" id="to" placeholder="To" />
-                    <input type="text" name="subject" id="subject" placeholder="subject" />
-                    <textarea name="paragraph_text" cols="50" rows="10"></textarea>                    <h1>subjuegtert</h1>
-                    <p>fddsfsddfdsalorem
+                    <input type="text" name="subject" id="subject" placeholder="Subject" />
+                    <textarea name="paragraph_text" cols="50" rows="14"></textarea>
 
-                    </p>
                 </form>
+            </div>
+            <div className="bottom-buttons">
+                <button className="send-btn">Send</button>
+                <button className="trash"><img src="assets\img\mail-icons\delete_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="delete" /></button>
             </div>
         </section>
     )
