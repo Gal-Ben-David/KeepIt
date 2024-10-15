45fea3afbb311f88792338f4023265e3b870e802
matan-ode <matanodentz@gmail.com>
2024-10-15 14:03:01 +0300

    Add icons hover show on MailList

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -2,14 +2,17 @@ const { Link } = ReactRouterDOM
 
 import { MailPreview } from "./MailPreview.jsx";
 
+
+
 export function MailList({ mails }) {
 
 
     return (
         <ul className="mail-list">
             {mails.map(mail =>
-                <li key={mail.id}>
+                <li className="mail-li" key={mail.id}>
                     <Link to={`/mail/${mail.id}`}><MailPreview mail={mail} /></Link>
+
                 </li>
             )}
         </ul>
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -26,6 +26,10 @@ export function MailPreview({ mail }) {
             </div>
 
             <div className="mail-date">{formatToDate(date)}</div>
+            <div className="mail-icons">
+                <button type="button" className="trash"><img src="assets\img\mail-icons\delete_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="delete" /></button>
+                {mail.isRead ? <button type="button" className="read"><img src="assets\img\mail-icons\mail_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="read" /></button> : <button type="button" className="unread"><img src="assets\img\mail-icons\drafts_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="unread" /></button>}
+            </div>
         </div>
     )
 }
