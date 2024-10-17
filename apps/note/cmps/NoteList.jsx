9f5f01f4c721310a5a9022defe0ae8111a046858
Gal Ben David <gal.benda3@gmail.com>
2024-10-17 10:49:52 +0300

    Transfer the note list rendering from NotePreview to NoteList

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,47 @@
-export function NoteList() {
-    return <div>note list</div>
+const { Fragment, useState, useEffect } = React
+
+export function NoteList({ notes, handleEditClick, onPinNote, onRemoveNote, onDuplicateNote, changeIsCheckedTodo }) {
+    return (
+        <Fragment>
+            <ul className="notes">
+                {notes.map(note => {
+
+                    return (<li key={note.id} style={{ backgroundColor: note.style.backgroundColor }} onClick={() => handleEditClick(note)}>
+
+                        {note.info.imgUrl && <img src={note.info.imgUrl} />}
+                        {note.info.videoUrl &&
+                            <iframe src={note.info.videoUrl} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+                            </iframe>}
+
+                        <div className={`title-and-info-content ${(note.info.imgUrl || note.info.videoUrl) ? 'absolute' : ''}`}>
+                            <button
+                                className={`pin-btn ${(note.info.imgUrl || note.info.videoUrl) ? 'absolute' : ''} ${(note.isPinned ? 'pinned' : '')}`}
+                                onClick={(ev) => { ev.stopPropagation(); onPinNote(note) }}>
+                                {note.isPinned ? <img src="assets/img/pin-full.png" /> : <img src="assets/img/pin-empty.png" />}
+                            </button>
+
+                            <h3>{note.noteTitle}</h3>
+                            <p>{note.info.txt}</p>
+                            {note.info.todos && note.info.todos.map((item, i) =>
+                                item && <label key={i} onClick={(ev) => { ev.stopPropagation() }} >
+                                    <input
+                                        type="checkbox"
+                                        checked={item.isChecked || false}
+                                        onChange={() => changeIsCheckedTodo(i, note)} />
+                                    <span className="todo-text">{item.txt}</span>
+                                </label>
+                            )}
+                        </div>
+
+                        <div className="toolbar">
+                            <button title="Delete Note" onClick={(ev) => { ev.stopPropagation(); onRemoveNote(note.id) }}><i className="fa-solid fa-trash"></i></button>
+                            <button title="Edit Note" onClick={(ev) => { ev.stopPropagation(); handleEditClick(note) }}><i className="fa-regular fa-pen-to-square"></i></button>
+                            <button title="Duplicate Note" onClick={(ev) => { ev.stopPropagation(); onDuplicateNote(note) }}><i className="fa-regular fa-clone"></i></button>
+                        </div>
+                    </li>)
+                })}
+            </ul>
+
+        </Fragment>
+    )
 }
--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,6 +1,8 @@
 import { NoteEdit } from '../cmps/NoteEdit.jsx'
 import { Modal } from '../cmps/Modal.jsx'
 import { noteService } from '../services/note.service.js'
+import { NoteList } from '../cmps/NoteList.jsx'
+import { showErrorMsg, showSuccessMsg } from "../../../services/event-bus.service.js"
 
 const { Fragment, useState, useEffect } = React
 
@@ -67,87 +69,20 @@ export function NotePreview({ notes, onRemoveNote, loadNotes, onPinNote, onDupli
         <Fragment>
             <section className={`pinned-notes ${pinnedDisplay}`}>
                 <h4>Pinned</h4>
-                <ul className="notes">
-                    {notesPreview.filter(note => note.isPinned).map(note => {
-
-                        return (<li key={note.id} style={{ backgroundColor: note.style.backgroundColor }} onClick={() => handleEditClick(note)}>
-
-                            {note.info.imgUrl && <img src={note.info.imgUrl} />}
-                            {note.info.videoUrl &&
-                                <iframe src={note.info.videoUrl} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-                                </iframe>}
-
-                            <div className={`title-and-info-content ${(note.info.imgUrl || note.info.videoUrl) ? 'absolute' : ''}`}>
-                                <button
-                                    className={`pin-btn ${(note.info.imgUrl || note.info.videoUrl) ? 'absolute' : ''} ${(note.isPinned ? 'pinned' : '')}`}
-                                    onClick={(ev) => { ev.stopPropagation(); onPinNote(note) }}>
-                                    {note.isPinned ? <img src="assets/img/pin-full.png" /> : <img src="assets/img/pin-empty.png" />}
-                                </button>
-
-                                <h3>{note.noteTitle}</h3>
-                                <p>{note.info.txt}</p>
-
-                                {note.info.todos && note.info.todos.filter(item => item).map((item, i) =>
-                                    <label key={i} onClick={(event) => { event.stopPropagation() }} >
-                                        <input
-                                            type="checkbox"
-                                            onClick={(event) => { event.stopPropagation() }} />
-                                        <span style={{ pointerEvents: 'none' }}>{item}</span>
-                                    </label>
-                                )}
-                            </div>
-
-                            <div className="toolbar">
-                                <button title="Delete Note" onClick={(ev) => { ev.stopPropagation(); onRemoveNote(note.id) }}><i className="fa-solid fa-trash"></i></button>
-                                <button title="Edit Note" onClick={(ev) => { ev.stopPropagation(); handleEditClick(note) }}><i className="fa-regular fa-pen-to-square"></i></button>
-                                <button title="Duplicate Note" onClick={(ev) => { ev.stopPropagation(); onDuplicateNote(note) }}><i className="fa-regular fa-clone"></i></button>
-                            </div>
-                        </li>)
-                    })}
-                </ul>
-
+                <NoteList notes={notesPreview.filter(note => note.isPinned)}
+                    handleEditClick={handleEditClick}
+                    onPinNote={onPinNote}
+                    onRemoveNote={onRemoveNote}
+                    onDuplicateNote={onDuplicateNote} />
             </section>
 
             <section className="unPinned-notes">
-                <h1 className={pinnedDisplay ? 'show' : ''}>Other notes</h1>
-                <ul className="notes">
-                    {notesPreview.filter(note => !note.isPinned).map(note =>
-                        <li key={note.id} style={{ backgroundColor: note.style.backgroundColor }} onClick={() => handleEditClick(note)}>
-
-                            {note.info.imgUrl && <img src={note.info.imgUrl} />}
-                            {note.info.videoUrl &&
-                                <iframe src={note.info.videoUrl} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-                                </iframe>}
-
-                            <div className={`title-and-info-content ${(note.info.imgUrl || note.info.videoUrl) ? 'absolute' : ''}`}>
-                                <button
-                                    className={`pin-btn ${(note.info.imgUrl || note.info.videoUrl) ? 'absolute' : ''} ${(note.isPinned ? 'pinned' : '')}`}
-                                    onClick={(ev) => { ev.stopPropagation(); onPinNote(note) }}>
-                                    {note.isPinned ? <img src="assets/img/pin-full.png" /> : <img src="assets/img/pin-empty.png" />}
-                                </button>
-
-                                <h3>{note.noteTitle}</h3>
-                                <p>{note.info.txt}</p>
-
-                                {note.info.todos && note.info.todos.map((item, i) =>
-                                    item && <label key={i} onClick={(ev) => { ev.stopPropagation() }} >
-                                        <input
-                                            type="checkbox"
-                                            checked={item.isChecked || false}
-                                            onChange={() => changeIsCheckedTodo(i, note)} />
-                                        <span className="todo-text">{item.txt}</span>
-                                    </label>
-                                )}
-                            </div>
-
-                            <div className="toolbar">
-                                <button title="Delete Note" onClick={(ev) => { ev.stopPropagation(); onRemoveNote(note.id) }}><i className="fa-solid fa-trash"></i></button>
-                                <button title="Edit Note" onClick={(ev) => { ev.stopPropagation(); handleEditClick(note) }}><i className="fa-regular fa-pen-to-square"></i></button>
-                                <button title="Duplicate Note" onClick={(ev) => { ev.stopPropagation(); onDuplicateNote(note) }}><i className="fa-regular fa-clone"></i></button>
-                            </div>
-                        </li>
-                    )}
-                </ul>
+                <NoteList notes={notesPreview.filter(note => !note.isPinned)}
+                    handleEditClick={handleEditClick}
+                    onPinNote={onPinNote}
+                    onRemoveNote={onRemoveNote}
+                    onDuplicateNote={onDuplicateNote}
+                    changeIsCheckedTodo={changeIsCheckedTodo} />
             </section>
 
             {isEditModalOpen && (
