bd24c5466710011c279afb6dd0aa6708474409d0
Gal Ben David <gal.benda3@gmail.com>
2024-10-19 11:32:53 +0300

    Update the About-page styling

--- pages/About.jsx
+++ pages/About.jsx
@@ -19,19 +19,35 @@ export function About() {
                 />
             </div>
 
+            <div className="team-lead">
+                <div className="profile-img"><img src="assets/img/team-batel.png" /></div>
+
+                <ul className="contact-info">
+                    <h2>Batel Katiei</h2>
+                    <h3>Team Lead</h3>
+                    <li> <i className="fa-regular fa-envelope"></i><span>batelkatiei1@gmail.com</span></li>
+                </ul>
+            </div>
+
             <article className="team-data">
                 <div className="team-member">
-                    <h1>Matan Odentz</h1>
+                    <div className="team-member-description">
+                        <h2>Matan Odentz</h2>
+                        <h3>App Developer</h3>
+                    </div>
                     <div className="profile-img"><img src="assets/img/team-matan.png" /></div>
                     <ul className="contact-info">
                         <hr />
                         <li> <i className="fa-solid fa-phone"></i><span>+972-54-445-42430</span></li>
-                        <li> <i className="fa-regular fa-envelope"></i><span>matanodz@gmail.com</span></li>
+                        <li> <i className="fa-regular fa-envelope"></i><span>matanodentz@gmail.com</span></li>
                     </ul>
                 </div>
 
                 <div className="team-member">
-                    <h1>Gal Ben David</h1>
+                    <div className="team-member-description">
+                        <h2>Gal Ben David</h2>
+                        <h3>App Developer</h3>
+                    </div>
                     <div className="profile-img"><img src="assets/img/team-gal.png" /></div>
 
                     <ul className="contact-info">
