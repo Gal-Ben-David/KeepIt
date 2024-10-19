6d3ae2ba0512bd114691a7c625d76a25bf5b807c
matan-ode <matanodentz@gmail.com>
2024-10-19 21:04:16 +0300

    Add burger menu for small mq

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,6 +1,25 @@
 const { Link, NavLink, Fragment } = ReactRouterDOM
+const { useRef } = React
 
 export function AppHeader() {
+
+    const navRef = useRef()
+    const backdropRef = useRef()
+
+    function toggleMenu() {
+        // pointer-events: none;
+        // opacity: 1;
+        
+        // navRef.current.style.opacity = 1
+        // navRef.current.style.pointerEvents = 'all'
+        navRef.current.classList.toggle('show')
+        backdropRef.current.classList.toggle('hide')
+    }
+
+    function onBackdrop(){
+        navRef.current.classList.remove('show')
+        backdropRef.current.classList.add('hide')
+    } 
 
     return <header className="app-header">
         <NavLink to="/">
@@ -9,11 +28,25 @@ export function AppHeader() {
                 <h2>Appsus</h2>
             </div>
         </NavLink>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
+        <div onClick={toggleMenu} className="apps-icon hide"><img src="assets\img\apps_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="apps" /></div>
+        <nav ref={navRef}>
+            <NavLink to="/" onClick={onBackdrop}>
+                <div className="nav-word">Home</div>
+                <div className="nav-logo hide"><img src="assets\img\home_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="home" /></div>
+            </NavLink>
+            <NavLink to="/about" onClick={onBackdrop}>
+                <div className="nav-word">About</div>
+                <div className="nav-logo hide"><img src="assets\img\group_24dp_202124_FILL0_wght400_GRAD0_opsz24.png" alt="about" /></div>
+            </NavLink>
+            <NavLink to="/mail" onClick={onBackdrop}>
+                <div className="nav-word">Mail</div>
+                <div className="nav-logo hide"><img src="assets\img\mail-icons\gmail-logo.png" alt="gmail" /></div>
+            </NavLink>
+            <NavLink to="/note" onClick={onBackdrop}>
+                <div className="nav-word">Note</div>
+                <div className="nav-logo hide"><img src="assets\img\keeps.png" alt="notes" /></div>
+            </NavLink>
         </nav>
+        <div onClick={onBackdrop} ref={backdropRef} className="menu-backdrop hide"></div>
     </header>
 }
