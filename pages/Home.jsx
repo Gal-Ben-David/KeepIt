c1f676c955a7995abbe29911869396e6e9c16e33
Gal Ben David <gal.benda3@gmail.com>
2024-10-19 10:52:09 +0300

    Add styling to the home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,42 @@
+const { useEffect } = React
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
-    </section>
+
+    useEffect(() => {
+        document.body.style.backgroundColor = '#FFFFFF'
+    }, [])
+
+    return (
+        <section className="home">
+
+            {/* <div className="animation-container">
+                <video
+                    src="assets/animation/horse.mp4"
+                    width="100"
+                    autoPlay
+                    // controls
+                    loop
+                />
+            </div> */}
+
+            <div className="welcome-area">
+                <h1>Appsus</h1>
+
+                <div className="welcome-message">
+                    <p>Welcome to Appsus 👋 </p>
+                    <p>  Your all-in-one solution for staying organized and productive. </p>
+                    <p>  From managing emails to capturing notes, </p>
+                    <p>  Appsus makes it easy to keep everything in one place!</p>
+                </div>
+            </div>
+
+            <div className="apps-container">
+                <h2>Our Apps</h2>
+                <div className="logos">
+                    <div className="app-logo"> <img src="assets/img/mail-icons/gmail-logo.png" /></div>
+                    <div className="app-logo">  <img src="assets/img/keeps.png" /></div>
+                </div>
+            </div>
+        </section>
+    )
 }
